26bea94ebfe4feedd5f214becb09b1696cda7b78
lonegunman <lonegunmanb@hotmail.com>
2018-10-31 18:45:31 +0800

    support two singers

--- golang/main.go
+++ golang/main.go
@@ -1,20 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var chorus = []*singer{
+	NewSinger("fly", ""),
+	NewSinger("spider", "That wriggled and wiggled and tickled inside her."),
+}
+
+func formUp(){
+	for i, s := range chorus {
+		if i > 0 {
+			s.sibling = chorus[i-1]
+		}
+	}
+}
 
 type singer struct {
 	name   string
+	overtureTemplate string
 	writer func(song string)
+	sibling *singer
 }
 
-func NewSinger(name string) *singer {
-	return &singer{name: name}
+func NewSinger(name string, overtureTemplate string) *singer {
+	return &singer{name: name, overtureTemplate:overtureTemplate}
 }
 
-func (a *singer) Sing() {
-	song := `There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!`
-	a.writer(song)
+func (s *singer) Sing(writer func(sing string)) {
+	s.writer = writer
+	if s.sibling != nil{
+		s.sibling.Sing(writer)
+		s.writer("\n")
+	}
+
+	s.selfIntroduceSing()
+	s.preludeSing()
+	s.chorusSing()
+	s.postludeSing()
+}
+
+func (s *singer) selfIntroduceSing() {
+	s.writer(fmt.Sprintf("There was an old lady who swallowed a %s", s.name))
+	delimiter := ";"
+	if s.sibling == nil {
+		delimiter = "."
+	}
+	s.writer(delimiter)
+	s.writer("\n")
+}
+
+func (s *singer) preludeSing() {
+	if s.sibling != nil {
+		preludeSong := ""
+		if strings.Contains(s.overtureTemplate, "%s") {
+			preludeSong = fmt.Sprintf(s.overtureTemplate, s.name)
+		} else {
+			preludeSong = s.overtureTemplate
+		}
+		s.writer(preludeSong)
+		s.writer("\n")
+	}
+}
+
+func (s *singer) chorusSing() {
+	if s.sibling != nil {
+		s.writer(fmt.Sprintf("She swallowed the %s to catch the %s;", s.name, s.sibling.name))
+		s.writer("\n")
+		s.sibling.chorusSing()
+	}
+}
+func (s *singer) postludeSing() {
+	s.writer("I don't know why she swallowed a fly - perhaps she'll die!")
 }
 
 func main() {
--- golang/sing_test.go
+++ golang/sing_test.go
@@ -6,17 +6,52 @@ import (
 	"testing"
 )
 
-func Test_SingFly(t *testing.T) {
+func Test_SingleSinger(t *testing.T) {
 	song := `There was an old lady who swallowed a fly.
 I don't know why she swallowed a fly - perhaps she'll die!`
+	formUp()
+	sb, writer := buildWriter()
+	fly := chorus[0]
+	fly.Sing(writer)
 
+	assert.Equal(t, song, sb.String())
+}
+
+func Test_DoubleSinger(t *testing.T){
+	song := `There was an old lady who swallowed a fly.
+I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a spider;
+That wriggled and wiggled and tickled inside her.
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!`
+	formUp()
+	sb, writer := buildWriter()
+
+	spider := chorus[1]
+	spider.Sing(writer)
+
+	assert.Equal(t, song, sb.String())
+}
+
+//func Test_TripleSinger(t *testing.T){
+//	song := `There was an old lady who swallowed a fly.
+//I don't know why she swallowed a fly - perhaps she'll die!
+//There was an old lady who swallowed a spider;
+//That wriggled and wiggled and tickled inside her.
+//She swallowed the spider to catch the fly;
+//I don't know why she swallowed a fly - perhaps she'll die!
+//There was an old lady who swallowed a bird;
+//How absurd to swallow a bird.
+//She swallowed the bird to catch the spider,
+//She swallowed the spider to catch the fly;
+//I don't know why she swallowed a fly - perhaps she'll die!`
+//
+//}
+
+func buildWriter() (*strings.Builder, func(sing string)) {
 	stringBuilder := &strings.Builder{}
 	writer := func(sing string) {
 		stringBuilder.WriteString(sing)
 	}
-	fly := NewSinger("fly")
-	fly.writer = writer
-	fly.Sing()
-
-	assert.Equal(t, song, stringBuilder.String())
+	return stringBuilder, writer
 }
